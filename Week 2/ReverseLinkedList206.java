269b71c5ef836bd3db4f3f2e8fbc832e459cd196
Ky <kylenneji887@gmail.com>
2022-03-02 12:25:24 -0500

    added folder for week 2

---
+++ Week 2/ReverseLinkedList206.java
@@ -0,0 +1,13 @@
+public class ReverseLinkedList206 {
+    public ListNode reverseList(ListNode head){
+
+        return head; 
+
+
+
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
