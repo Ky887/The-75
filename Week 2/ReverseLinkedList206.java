a0d6553da295b2cdd883e8095fe809650ebc7e32
Ky <kylenneji887@gmail.com>
2022-03-04 17:51:44 -0500

    testing branch changes with code

--- Week 2/ReverseLinkedList206.java
+++ Week 2/ReverseLinkedList206.java
@@ -1,5 +1,5 @@
 public class ReverseLinkedList206 {
-    public ListNode reverseList(ListNode head){
+    public static ListNode reverseList(ListNode head){
 
         return head; 
 
@@ -7,7 +7,18 @@ public class ReverseLinkedList206 {
 
     }
     public static void main(String[] args) {
-        
-    }
+        ListNode head = new ListNode(1); 
+        ListNode curr = head; 
+        for(int i = 2; i < 6; i++){
+            ListNode blah = new ListNode(i); 
+            curr.next = blah; 
+            curr = curr.next; 
+        }
+       ListNode ans = reverseList(head); 
+        while(ans != null){
+            System.out.println(head.val);
+            head = head.next;      
+        }
     
+    }
 }
