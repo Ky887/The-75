b0f80f3d48a74f5bcbc9609a5491d74f7c65721b
Ky <kylenneji887@gmail.com>
2022-03-07 15:23:09 -0500

    fixed typo

--- Week 2/LinkedListCycle141.java
+++ Week 2/LinkedListCycle141.java
@@ -3,7 +3,7 @@ public class LinkedListCycle141 {
      Method 1: Floyd's Tortoise & Hare
         Slow and fast pointer method. slow pointer moves at 1x while fast at 2x.
         During each iteration fast pointer will be one position closer (+1 -2 = -1). 
-        So this alglorith is designed by N-1 = O(n). It will take at most n times for 
+        So this alglorithm is defined by N-1 = O(n). It will take at most n times for 
         slow and faster pointers to be at the same position. 
 
         Two pointers at diffenet speeds, if they meet there's a loop
