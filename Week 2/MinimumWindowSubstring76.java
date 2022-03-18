0fd7e0ed5b3308a27f20760ade86f0117c2bdafe
Ky <kylenneji887@gmail.com>
2022-03-18 12:00:54 -0400

    added week 3

--- Week 2/MinimumWindowSubstring76.java
+++ Week 2/MinimumWindowSubstring76.java
@@ -1,14 +1,24 @@
 
 public class MinimumWindowSubstring76 {
+    /* 
+    Method 1:
+    Sliding window. Can use array histogram or HashMap --> to update HashMap use code: int count = windowCounts.getOrDefault(c, 0); windowCounts.put(c, count + 1);
+    1. Start with two pointers left and right pointing to first element
+    2. Use the right pointer to expand the window
+    3. Once we have a window with all required characters shrink the window from left, while updating minLen
+        You know when all the required chars are in window with a matchCounter
+        if added char is less or equal to the the charCount of wantHist then you know u made a step in the right direction. (match++)
+        shrink left must take into account leftPointer, currHist and match. Adjust those approprietly with respect to the chars lost from window. 
+    4. If the window is not desirable anymore, repeat step 2 and expand the window untill loopinvariant restored. 
+    */ 
     public static String minWindow(String s, String t){
         if(s.length() < t.length()){
             return ""; 
         }
-       // s = s.toUpperCase(); 
-       // t = t.toUpperCase(); 
+        
         int left = 0, right = 0; 
         int match = 0; 
-        int matchTarget = t.length();
+        int matchTarget = (int) t.chars().distinct().count();
         int minLen = Integer.MAX_VALUE;  
         String result = ""; 
 
@@ -25,7 +35,7 @@ public class MinimumWindowSubstring76 {
            //if added correct char to window, update match, if there is a full match update minLen. 
            int curChar = s.charAt(right)-'A';
            current[curChar]++;
-           if(want[curChar] >=  current[curChar]){
+           if(want[curChar] ==  current[curChar]){
                 match++; 
            }
            while(match == matchTarget){
@@ -33,6 +43,7 @@ public class MinimumWindowSubstring76 {
                    minLen = (right -left) +1; 
                    result = s.substring(left, right+1); 
                } 
+               // shrinking window 
                int leftMostChar = s.charAt(left) - 'A'; 
                current[leftMostChar]--; 
                left++; 
---
+++ Week 3/InvertBinaryTree226.java
@@ -0,0 +1,6 @@
+public class InvertBinaryTree226 {
+
+    public static void main(String[] args) {
+        System.out.println("hello World!!!");
+    }
+}
